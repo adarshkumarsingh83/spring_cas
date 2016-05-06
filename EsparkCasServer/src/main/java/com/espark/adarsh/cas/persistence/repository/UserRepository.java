776bb3c0d2ccb57747cc2dab5bfa35b350622afe
adarkuma <adarkuma@cisco.com>
2016-05-06 10:15:35 +0530

    adding

--- EsparkCasServer/src/main/java/com/espark/adarsh/cas/persistence/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.espark.adarsh.cas.persistence.repository;
-
-
-import com.espark.adarsh.cas.persistence.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
